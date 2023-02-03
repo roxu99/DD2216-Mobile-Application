431105d101d8b3ee0a8b3134a7d71c5b61c01b62
roxu99 <rxu0598@hotmail.fr>
2023-02-03 15:19:54 +0100

    LoginScreen first version

--- vite-drink-it/src/App.jsx
+++ vite-drink-it/src/App.jsx
@@ -2,31 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import axios from 'axios'
+
+import LoginPage from '../components/LoginPage'
+
 function App() {
   const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('')
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src="/Logo-without-bg.svg" className="logo" alt="Drint it logo" />
+      <h1 class="app-name">Drint'it</h1>
+      {/* <h1> {quote} </h1> */}
+    <LoginPage></LoginPage>
+
+
+{/*     <button onClick={() => axios.get('https:/api.kanye.rest').then(
+      result => setQuote(result.data.quote)
+    )}>
+      Click me
+    </button> */}
+
+
     </div>
   )
 }
