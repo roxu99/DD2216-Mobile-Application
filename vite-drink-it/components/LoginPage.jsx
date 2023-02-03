c87b47c5d54e75bf0b12082500b041600cc1a828
Vianey Darsel <vdarsel@live.fr>
2023-02-03 22:03:34 +0100

    Add Bootstrap and responsive design for Computer and Mobile

--- vite-drink-it/components/LoginPage.jsx
+++ vite-drink-it/components/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Col from 'react-bootstrap/Col';
 
 // import { View, Image, StyleSheet, Dimensions, Alert } from 'react-native';
 // import { Block, Button, Input, NavBar, Text } from 'galio-framework';
@@ -10,31 +11,31 @@ import { Link } from 'react-router-dom'
 
 
 export default function LoginPage() {
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Who are you?</h2>
-            <form action="/home">
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sign_btn" type="submit">SignIn</button>
-                    <button id="sub_btn" type="submit">Login</button>
-                    {/* <Link to="/forget-password"><label className="right-label">Forget password?</label></Link> */}
-                </p>
-                <p>
-                    <label className="right-label">Forget password?</label>
-                </p>
-            </form>
-            {/* <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer> */}
-        </div>
-    )
-}
+return (
+    <Col xs={12}>
+    <h2>Who are you?</h2>
+    <form action="/home" id="signup">
+        <p>
+            <label>Email address</label><br/>
+            <input type="text" name="first_name" required />
+        </p>
+        <p>
+            <label>Password</label><br/>
+            <input type="password" name="password" required />
+        </p>
+        <p>
+            <button id="sign_btn" type="submit">SignIn</button>
+            <button id="sub_btn" type="submit">Login</button>
+            {/* <Link to="/forget-password"><label className="right-label">Forget password?</label></Link> */}
+        </p>
+        <p>
+            <label className="right-label">Forget password?</label>
+        </p>
+    </form>
+    {/* <footer>
+     <p>First time? <Link to="/register">Create an account</Link>.</p>
+     <p><Link to="/">Back to Homepage</Link>.</p>
+     </footer> */}
+    </Col>
+        )
+        }
--- vite-drink-it/src/App.jsx
+++ vite-drink-it/src/App.jsx
@@ -1,32 +1,48 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import axios from 'axios'
 
 import LoginPage from '../components/LoginPage'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [quote, setQuote] = useState('')
+        function App() {
+                    const [count, setCount] = useState(0)
+                    const [quote, setQuote] = useState('')
 
-  return (
-    <div className="App">
-      <img src="/Logo-without-bg.svg" className="logo" alt="Drint it logo" />
-      <h1 class="app-name">Drint'it</h1>
-      {/* <h1> {quote} </h1> */}
+                            return (
+                                    <Container fluid className="App content">
+    <Row className="justify-content-center">
+        <Col lg=
+                            {3} md={5} sm={8} xs={12}>
+        <img src="/Logo-without-bg.svg" className="logo" alt="Drink'it logo" />
+        </Col>
+    </Row>
+    <Row>
+        <h1 className="app-name">Drink'it</h1>
+
+        
+                            {/* <h1> {quote} </h1> */}
+                            
+
+    </Row>                           
     <LoginPage></LoginPage>
 
 
-{/*     <button onClick={() => axios.get('https:/api.kanye.rest').then(
-      result => setQuote(result.data.quote)
-    )}>
-      Click me
-    </button> */}
 
 
-    </div>
-  )
-}
+    
+                            {/*     <button onClick={() => axios.get('https:/api.kanye.rest').then(
+                             result => setQuote(result.data.quote)
+                             )}>
+                             Click me
+                             </button> */}
+                            
+</Container>
+                            )
+        }
 
 export default App
