89612c2aabdd1fbef8b4ca016786ac8b05851ef4
roxu99 <rxu0598@hotmail.fr>
2023-02-04 15:19:31 +0100

    change indent

--- vite-drink-it/src/App.jsx
+++ vite-drink-it/src/App.jsx
@@ -9,40 +9,26 @@ import axios from 'axios'
 
 import LoginPage from '../components/LoginPage'
 
-        function App() {
-                    const [count, setCount] = useState(0)
-                    const [quote, setQuote] = useState('')
+function App() {
+    const [count, setCount] = useState(0)
+    const [quote, setQuote] = useState('')
 
-                            return (
-                                    <Container fluid className="App content">
-    <Row className="justify-content-center">
-        <Col lg=
-                            {3} md={5} sm={8} xs={12}>
-        <img src="/Logo-without-bg.svg" className="logo" alt="Drink'it logo" />
-        </Col>
-    </Row>
-    <Row>
-        <h1 className="app-name">Drink'it</h1>
+    return (
+        <Container fluid className="App content">
+            <Row className="justify-content-center">
+                <Col lg={3} md={5} sm={8} xs={12}>
+                    <img src="/Logo-without-bg.svg" className="logo" alt="Drink'it logo" />
+                </Col>
+            </Row>
 
-        
-                            {/* <h1> {quote} </h1> */}
+            <Row>
+                <h1 className="app-name">Drink'it</h1>
+            </Row>
+
+            <LoginPage></LoginPage>
                             
-
-    </Row>                           
-    <LoginPage></LoginPage>
-
-
-
-
-    
-                            {/*     <button onClick={() => axios.get('https:/api.kanye.rest').then(
-                             result => setQuote(result.data.quote)
-                             )}>
-                             Click me
-                             </button> */}
-                            
-</Container>
-                            )
-        }
+        </Container>
+    )
+}
 
 export default App
