108ba7e2e3831e97cdbcc089f2acc1d2bd352331
Vianey Darsel <vdarsel@live.fr>
2023-02-07 00:38:30 +0100

    NavBar for Mobile

--- vite-drink-it/components/NavBar.jsx
+++ vite-drink-it/components/NavBar.jsx
@@ -1,14 +1,25 @@
+import { MapFill } from 'react-bootstrap-icons'
+import Col from 'react-bootstrap/esm/Col'
+import Container from 'react-bootstrap/esm/Container'
+import Row from 'react-bootstrap/esm/Row'
 import { Link, NavLink } from 'react-router-dom'
 import { AuthProvider } from '../utils/authentification'
 
 export const NavBar = () => {
     return (
-        <nav>
-            <Link to='/MapBars'>MapBars</Link>
-            <Link to='/ListBars'>ListBars</Link>
-            <NavLink to='/MyFavorites'>MyFavotiteBars</NavLink>
-
-            {/* {!auth.user && (<Link to='/login'>MyFavotiteBars</Link>)} */}
-        </nav>
+        <Container fluid id="NavBar" className='d-md-none'>
+            <Row>
+                <Col >
+                    <Link to='/MapBars'><MapFill /></Link>
+                </Col>
+                <Col >
+                    <Link to='/ListBars'>ListBars</Link>
+                </Col>
+                <Col >
+                    <NavLink to='/MyFavorites'>MyFavotiteBars</NavLink>
+                </Col>
+                {/* {!auth.user && (<Link to='/login'>MyFavotiteBars</Link>)} */}
+            </Row>
+        </Container>
     )
 }
