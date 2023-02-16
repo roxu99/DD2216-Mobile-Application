014eac83bbe95610242c1802821e153fa0c8e222
Vianey Darsel <vdarsel@live.fr>
2023-02-16 16:27:23 +0100

    Change Data Structure + Fav button on bar + Fav Page

--- vite-drink-it/components/BarPages/BarGenericPage.jsx
+++ vite-drink-it/components/BarPages/BarGenericPage.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { NavBarMobile, NavBarComputer } from '../NavBar';
+import {barNames,barLiked} from '../Data'
+import { Heart, HeartFill } from 'react-bootstrap-icons';
 
 export default function BarGeneric() {
     const param = useParams()
     const barId = param.barId
+    const [liked,ChangeLike] = useState(barLiked[barId])
+
+
+    function LikeBar(){
+        barLiked[barId] = 1- barLiked[barId]
+        ChangeLike(barLiked[barId])
+    }
+
     // const barInfo = get_Info_Bar(barId)
-    const barNames = ["Carmen", "Baras Backe", "Lion's","BlackElk"];
 
     return (
         <div className='Page'>
@@ -21,6 +30,11 @@ export default function BarGeneric() {
                     <Col md={10} sm={12} className='Map'>
                         <h2>Bar {barNames.at(barId)}</h2>
                     </Col>
+                </Row>
+                <Row>
+                    {
+                        liked == 1 ? <HeartFill onClick={LikeBar}></HeartFill> : <Heart onClick={LikeBar}></Heart>
+                    }
                 </Row>
             </Container>
             <NavBarMobile></NavBarMobile>
