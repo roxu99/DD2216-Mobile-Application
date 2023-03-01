3d1880da35e05e612c98cd30e5aa9f002db78567
Vianey Darsel <vdarsel@live.fr>
2023-03-01 13:21:23 +0100

    Add authorisation

--- vite-drink-it/utils/authentification.jsx
+++ vite-drink-it/utils/authentification.jsx
@@ -4,16 +4,19 @@ const AuthContext = createContext(null)
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    const [autorisation, setAuthorisation] = useState(null)
 
-    const login = user => {
+    const login = (user,auto) => {
         setUser(user)
+        setAuthorisation(auto)
     }
 
     const logout = () => {
         setUser(null)
+        setAuthorisation(0)
     }
 
-    return <AuthContext.Provider value={{user, login, logout}}>
+    return <AuthContext.Provider value={{user, autorisation, login, logout}}>
         {children}
     </AuthContext.Provider>
 }
