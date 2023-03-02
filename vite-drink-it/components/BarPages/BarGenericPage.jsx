7b8f89016dc76f26a637eb212c7bf0b2c541e1e2
Vianey Darsel <vdarsel@live.fr>
2023-03-02 12:05:59 +0100

    Suggestion implemented

--- vite-drink-it/components/BarPages/BarGenericPage.jsx
+++ vite-drink-it/components/BarPages/BarGenericPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState}from 'react'
+import React, { useCallback, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,24 +7,27 @@ import { NavBarMobile, NavBarComputer } from '../NavBar';
 import '../css/BarGenericPage.css';
 import { HandThumbsUp, HandThumbsUpFill, HandThumbsDown, HandThumbsDownFill } from 'react-bootstrap-icons'
 import barImage0 from './img/bar0.jpg'
-import { Modal, Table } from 'react-bootstrap';
-import { popup } from 'leaflet';
+import { Button, InputGroup, Modal, Table } from 'react-bootstrap';
 //import BARIMAGES from './images';
 
 import { Heart, HeartFill } from 'react-bootstrap-icons';
-import { barCheapestPrice,barLiked, barNames, suggestion, correspondVolume } from '../Data';
+import { barCheapestPrice, barLiked, barNames, suggestion, correspondVolume, barPosition } from '../Data';
 import { useAuth } from '../../utils/authentification';
+import { popup } from 'leaflet';
 
 export default function BarGeneric() {
     const param = useParams()
     const barId = param.barId
     const context = useAuth()
+    const [suggestionOpen, ClickSuggestion] = useState(false)
+    const [PriceSuggestion, ChangePriceSuggestion] = useState('')
+    const [VolumeSuggestion, ChangeVolumeSuggestion] = useState('')
 
     if (context.autorisation < 1) {
         const navigate = useNavigate()
         navigate("/login")
     }
-     // const barInfo = get_Info_Bar(barId)
+    // const barInfo = get_Info_Bar(barId)
     // const barNames = ["Carmen", "Baras Backe", "Lion Bar","BlackElk"];
     // const barCheapestPrice = [39, 42, 49, 49]
     // const correspondVolume = [40, 40, 50, 50]
@@ -32,11 +35,11 @@ export default function BarGeneric() {
     //                         "Götgatan 33, 116 21 Stockholm", 
     //                         "Sveavägen 74, 113 59 Stockholm",
     //                         "Sveavägen 61, 113 59 Stockholm" ]
-    const suggBool = true ? suggestion[barId]!='' : false
+    const suggBool = true ? suggestion[barId] != '' : false
 
-    const [liked,ChangeLike] = useState(barLiked[barId])
-    function LikeBar(){
-        barLiked[barId] = 1- barLiked[barId]
+    const [liked, ChangeLike] = useState(barLiked[barId])
+    function LikeBar() {
+        barLiked[barId] = 1 - barLiked[barId]
         ChangeLike(barLiked[barId])
     }
 
@@ -45,14 +48,14 @@ export default function BarGeneric() {
     // const handleShow = () => setShow(true)
 
     function ShowOtherSuggestion(bool) {
-        if(bool==true) {
+        if (bool == true) {
             return (
                 <div id="suggestion-box" className="boxed">
-                <br></br>
-                <h4>Other suggestion</h4>     
+                    <br></br>
+                    <h4>Other suggestion</h4>
                     <h3><HandThumbsUp id="oth-thumb-left"></HandThumbsUp>
                         <mark className="red-price">{suggestion[barId][0]} kr/ {suggestion[barId][1]} cL</mark><HandThumbsDown id="oth-thumb-right"></HandThumbsDown></h3>
-                <br></br>
+                    <br></br>
                 </div>
             )
         }
@@ -66,8 +69,22 @@ export default function BarGeneric() {
         // elem.appendChild(form)
     }
 
-    function SuggestionFormPopUp() {
-        pop = window.open('/NewSuggestionForm');
+    function SuggestionForm() {
+        ClickSuggestion(!suggestionOpen)
+    }
+
+    function SubmitForm() {
+        SuggestionForm();
+        if (barCheapestPrice[barId] === "") {
+            barCheapestPrice[barId] = PriceSuggestion
+            correspondVolume[barId] = VolumeSuggestion
+        }
+        else {
+            const new_suggestion = []
+            new_suggestion.push(PriceSuggestion)
+            new_suggestion.push(VolumeSuggestion)
+            suggestion[barId] = new_suggestion
+        }
     }
 
 
@@ -77,15 +94,15 @@ export default function BarGeneric() {
             <Container className="Bar Content">
                 <Row>
                     <Col md={8} sm={12} xs={12} className='Bar information offset-md-3'>
-                        <h2 className="title">{barNames.at(barId)}                   
+                        <h2 className="title">{barNames.at(barId)}
                             {
                                 liked == 1 ? <HeartFill id="fav" onClick={LikeBar}></HeartFill> : <Heart id="fav" onClick={LikeBar}></Heart>
                             }
                         </h2>
 
                         {/* <img src={require('../../src/img/bar0.jpg').default} ></img> */}
-                        <img alt="Bar's image" src={barImage0} style={{height:'20vh'}}></img>
-                        
+                        <img alt="Bar's image" src={barImage0} style={{ height: '20vh' }}></img>
+
 
                         <Table id="table-generic" className="table">
                             <tbody className="borderless">
@@ -99,7 +116,7 @@ export default function BarGeneric() {
                                 </tr>
                                 <tr>
                                     <th scope="row">Price per liter</th>
-                                    <td>{barCheapestPrice[barId]/correspondVolume[barId]*100} kr/L</td>
+                                    <td>{barCheapestPrice[barId] / correspondVolume[barId] * 100} kr/L</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">Distance</th>
@@ -111,34 +128,62 @@ export default function BarGeneric() {
                                 </tr>
                             </tbody>
                         </Table>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={8} sm={12} xs={12} className='Bar information offset-md-3'>
 
-                        <button id="btn-suggestion" onClick={SuggestionFormPopUp} type="submit">Suggest new price</button>
-                        {/* <Modal>
+                        <button id="btn-suggestion" onClick={SuggestionForm} type="submit">Suggest new price</button>
+                    </Col>
+                </Row>
+                {suggestionOpen &&
+                    <Row>
+                        <form onSubmit={SubmitForm}>
+                            <Col md={8} sm={12} xs={12} className='offset-md-3'>
+                                <InputGroup className='justify-content-center'>
 
-                        </Modal> */}
+                                    <input className='input_suggestion' type={"number"} step="0.01" name="name" onChange={(e) => ChangePriceSuggestion(e.target.value)} placeholder='Price'></input>
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">kr</span>
+                                    </div>
+                                    <input className='input_suggestion' type={"number"} name="name" onChange={(e) => ChangeVolumeSuggestion(e.target.value)} placeholder='Volume'></input>
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">cL</span>
+                                    </div>
+                                    <div className="input-group-append">
+                                        <button type='submit' id="SubmitSuggestion">Submit</button>
+                                    </div>
+                                </InputGroup>
 
-                        <br></br><br></br>
-                        
+                            </Col>
+                        </form>
+                    </Row>
+                }
+
+                <br></br><br></br>
+                <Row>
+                    <Col md={8} sm={12} xs={12} className='Bar information offset-md-3'>
+
                         <h3>Validate the suggestion <HandThumbsUp id="val-thumb"></HandThumbsUp></h3>
                         <br></br>
-                        
+
                         <div id="suggestion-form"></div>
                         {ShowOtherSuggestion(suggBool)}
 
                         <br></br><br></br>
 
-                        <button>Go There</button>
+                        <a target="_blank"  href={"https://www.google.com/maps/place/"+barPosition[barId][0]+","+barPosition[barId][1]}><button>Go There</button></a>
 
                     </Col>
                 </Row>
-            </Container>
+            </Container >
             <NavBarMobile></NavBarMobile>
-        </div>
+        </div >
     )
 }
 
 
-function changeIcon(){
+function changeIcon() {
     return {
 
     }
